7071826f82bcff69de2f2c76520cbf4bc850531d
Emmanuel Allan <emmanuelallanmj@gmail.com>
2022-09-03 22:40:16 +0530

    Added delete operation

--- App.js
+++ App.js
@@ -12,6 +12,11 @@ export default function App() {
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
   }
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id != id);
+    });
+  }
 
   return (
     <View style={styles.appContainer}>
@@ -22,7 +27,13 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem itemData={itemData} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
     <View style={styles.goalItems}>
-      {/* rounded corners wont work in ios if the styles is included in text. But if we do so, we would lose white text color */}
-      <Text style={styles.goalText}>{props.itemData.item.text}</Text>
+      <Pressable
+        android_ripple={{ color: "#ddd" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressData }) => pressData && styles.pressedItem}
+      >
+        {/* rounded corners wont work in ios if the styles is included in text. But if we do so, we would lose white text color */}
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -15,12 +21,15 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItems: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
     color: "#fff",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
+    padding: 8,
     color: "#fff",
   },
 });
